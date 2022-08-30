4395968eb9fbfc284218d83f120aba2d5644dcf0
Aruming <0430kar@naver.com>
2022-08-30 16:38:12 +0900

    ManyToMany 관계로 해결

---
+++ src/main/java/com/ll/exam/qsl/interestKeyword/entity/InterestKeyword.java
@@ -0,0 +1,32 @@
+package com.ll.exam.qsl.interestKeyword.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InterestKeyword {
+    @Id
+    private String content;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof InterestKeyword)) return false;
+
+        InterestKeyword that = (InterestKeyword) o;
+
+        return content.equals(that.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return content.hashCode();
+    }
+}
